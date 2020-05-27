83ee9358b0a47d926098be545c039ff197a6614d
Saleh Mahagney <saleh.mahagney@ibm.com>
2020-05-27 18:48:06 +0300

    change axios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://ec2-3-123-2-43.eu-central-1.compute.amazonaws.com'
+  baseURL: 'http://ec2-18-157-126-147.eu-central-1.compute.amazonaws.com'
 });
 
 export default axiosInstance;
