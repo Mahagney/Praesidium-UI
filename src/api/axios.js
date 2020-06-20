5e866db1cfe4b08e2171a6ad33c7cbe04ea0b1e6
mroobert <robertmihairo94@gmail.com>
2020-06-20 18:25:52 +0300

    modify axios base url to ec2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://ec2-18-157-126-147.eu-central-1.compute.amazonaws.com'
-  baseURL:'http://localhost:5000'
+  baseURL: 'http://ec2-18-157-126-147.eu-central-1.compute.amazonaws.com'
 });
 
 export default axiosInstance;
- 
